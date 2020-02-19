4af4ba8901d434f1b42eeb53e5b0d7fb51df0a89
MateuszNowak <kontakt.mateusznowak@gmail.com>
2020-02-19 22:21:36 +0100

    Krok 3 - Utworzenie modułu Inviting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { InvitingModule } from './inviting/inviting.module';
 
 @Module({
-  imports: [],
+  imports: [InvitingModule],
   controllers: [],
   providers: [],
 })
